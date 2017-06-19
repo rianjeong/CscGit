adf90ccff4eca8d470f78092f478dc80491a7666
rianjeong <rianjeong@yahoo.com>
2017-06-19 14:25:25 -0700

    calculator challenge
    This is the commit for my simple calculator using c#

---
+++ challengeSimpleCalculator/challengeSimpleCalculator/Calculator1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator1.aspx.cs" Inherits="challengeSimpleCalculator.Calculator1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server" style="font-family: Arial, Helvetica, sans-serif">
+    
+    
+        <h1><strong>Simple Calculator</strong></h1>
+        <br />
+    
+    
+        First Value:&nbsp;&nbsp;
+        <asp:TextBox ID="firstValuebox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Second Value:&nbsp;
+        <asp:TextBox ID="secondValueBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        &nbsp;
+        <asp:Button ID="addButton" runat="server" Text="+" OnClick="addButton_Click" Width="22px" />
+        &nbsp;
+        <asp:Button ID="subtractButton" runat="server" Text="-" OnClick="subtractButton_Click" Width="21px" />
+        &nbsp;
+        <asp:Button ID="multiplcationButton" runat="server" Text="*" OnClick="multiplcationButton_Click" Width="22px" />
+        &nbsp;
+        <asp:Button ID="divideButton" runat="server" Text="/" Width="20px" OnClick="divideButton_Click" />
+        <br />
+        <br />   
+        <asp:Label ID="totalValue" runat="server" BackColor="#3399FF"></asp:Label>
+    
+       
+    </form>
+</body>
+</html>
---
+++ challengeSimpleCalculator/challengeSimpleCalculator/Calculator1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace challengeSimpleCalculator
+{
+    public partial class Calculator1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValuebox.Text;
+            string secondValue =secondValueBox.Text;
+
+            int result = int.Parse(firstValue) + int.Parse(secondValue);
+            totalValue.Text = result.ToString(); 
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValuebox.Text;
+            string secondValue = secondValueBox.Text;
+
+            int result = int.Parse(firstValue) - int.Parse(secondValue);
+            totalValue.Text = result.ToString();
+        }
+
+        protected void multiplcationButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValuebox.Text;
+            string secondValue = secondValueBox.Text;
+
+            int result = int.Parse(firstValue) * int.Parse(secondValue);
+            totalValue.Text = result.ToString();
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValuebox.Text;
+            string secondValue = secondValueBox.Text;
+
+            double result = double.Parse(firstValue) / double.Parse(secondValue);
+            totalValue.Text = result.ToString();
+        }
+    }
+}
